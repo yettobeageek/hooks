4f56d7dc5de71886880735d303c2f0b2fdbbe5aa
yettobeageek <manojgowdago@gmail.com>
2022-08-04 12:53:10 +0530

    commit

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -26,7 +26,7 @@ const AddTransaction = () => {
     <form onSubmit={onSubmit}>
         <div className="form-control">
             <label htmlFor="text">Text</label>
-            <input type="text" value={text} onChange={(e)=>setText(e.target.value)} placeholder='Enter text..' />
+            <input type="text" value={text} onChange={(e)=>setText(e.target.value)} placeholder='Add your new transaction..' />
         </div>
         <div className="form-control">
             <label htmlFor="amount">Amount <br />(negative-expense,positive-income)</label>
